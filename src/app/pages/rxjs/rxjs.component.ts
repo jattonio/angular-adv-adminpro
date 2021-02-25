06f6c120ddad16534f67e5139d019811b97c33db
Antonio Torres <jattonio@gmail.com>
2021-02-24 23:58:20 -0600

    Fin seccion 8

---
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-rxjs',
+  templateUrl: './rxjs.component.html',
+  styles: [
+  ]
+})
+export class RxjsComponent {
+
+  constructor() { 
+
+  }
+
+}
